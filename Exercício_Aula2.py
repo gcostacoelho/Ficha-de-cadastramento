24c1e5e5c920c21e3794b0ffab2330c3af5800bd
Gustavo <66639892+gcostacoelho@users.noreply.github.com>
2021-07-28 11:14:07 -0300

    Add files via upload

---
+++ Exercício_Aula2.py
@@ -0,0 +1,19 @@
+import sys
+arg = sys.argv
+print (arg)
+
+nome = input ('Insira seu nome: ')
+idade = input ('Insira sua idade: ')
+altura = input ('Insira sua altura: ')
+cpf = input ('Insira seu CPF: ')
+endereço = input ('Insira seu endereço: ')
+tel = input ('Insira seu telefone: ')
+
+
+print ('')
+print ('Nome: ', nome)
+print ('Idade: ', idade, )
+print ('Altura: ', altura)
+print ('CPF: ', cpf)
+print ('Endereço: ', endereço)
+print ('Telefone: ', tel)
